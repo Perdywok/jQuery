37a8872fd59f1b0bd300cedd77ce4d3bd0494693
Perdywok <kosras.96@gmail.com>
2018-01-27 13:27:41 +0200

    trying work with jQuery

---
+++ jQuery/Scripts/jQueryCode.js
@@ -0,0 +1,9 @@
+﻿    $(".animated").click(function () {
+        alert("don't touch my rectangles!!");
+    });
+    $(".animated").mouseenter(function () {
+        $(".animated").fadeTo("fast", 0.25);
+    });
+    $(".animated").mouseleave(function () {
+        $(".animated").fadeTo("fast", 1);
+    });
